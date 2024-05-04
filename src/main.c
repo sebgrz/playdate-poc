8f93c191bd6b734cce7648a2e804bc1d9db15ce8
Seba G <sebag.git@gmail.com>
2024-05-04 21:14:20 +0200

    feat: bitmaps

--- src/main.c
+++ src/main.c
@@ -2,14 +2,33 @@
 
 #include "pd_api.h"
 
+LCDBitmap *bitmap, *bitmap2;
+
 static int update(void *userdata);
 
 int eventHandler(PlaydateAPI *playdate, PDSystemEvent event, uint32_t arg) {
   if (event == kEventInit) {
+    // Load gfx
+    const char *err_no = NULL;
+    bitmap = playdate->graphics->loadBitmap("images/sprites", &err_no);
+    if (err_no != NULL) {
+      playdate->system->logToConsole("Err: %s", err_no);
+      return -1;
+    }
+
+    // Update
     playdate->system->logToConsole("%p", playdate);
     playdate->system->setUpdateCallback(update, playdate);
   }
   return 0;
 }
 
-static int update(void *userdata) { return 1; }
+static int update(void *userdata) {
+  PlaydateAPI *playdate = userdata;
+  playdate->system->drawFPS(10, 10);
+
+  playdate->graphics->drawBitmap(bitmap, 32, 32, kBitmapUnflipped);
+  playdate->graphics->tileBitmap(bitmap, 32, 0, 32, 32, kBitmapUnflipped);
+
+  return 1;
+}
