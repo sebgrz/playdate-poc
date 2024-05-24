0118039949d96bb1fac4984c867f773e7139a37a
Seba G <sebag.git@gmail.com>
2024-05-24 22:27:22 +0200

    draw only nearest intersection points

--- src/main_draw.c
+++ src/main_draw.c
@@ -16,13 +16,12 @@ const size_t WALLS_SIZE = sizeof(WALLS) / sizeof(WALLS[0]);
 
 PlaydateAPI *globalPlaydate;
 
-int playerX = 100, playerY = 100;
+int playerX = 100, playerY = 50;
 int positionAngle = 0;
 
-static bool getLineIntersection(float p0_x, float p0_y, float p1_x,
-                                  float p1_y, float p2_x, float p2_y,
-                                  float p3_x, float p3_y, float *i_x,
-                                  float *i_y);
+static bool getLineIntersection(float p0_x, float p0_y, float p1_x, float p1_y,
+                                float p2_x, float p2_y, float p3_x, float p3_y,
+                                float *i_x, float *i_y, float *distance);
 static int normalizeAngle(int angle);
 static int update(void *userdata);
 static int updateKey(PDButtons button, int down, uint32_t when, void *userdata);
@@ -92,19 +91,29 @@ static int update(void *userdata) {
     distanceY = (tempDistanceX - playerX) * sin(radAngle) +
                 (tempDistanceY - playerY) * cos(radAngle) + playerY;
 
-    playdate->graphics->drawLine(playerX, playerY, round(distanceX),
-                                 round(distanceY), 1, kColorBlack);
-
     // wall collisions
     int i;
-    float colX, colY;
+    float colX, colY, tempColX, tempColY, distance = FOV_LENGTH,
+                                          tempDistance = FOV_LENGTH;
     for (i = 0; i < WALLS_SIZE; i++) {
       wall = &WALLS[i];
       if (getLineIntersection(playerX, playerY, distanceX, distanceY,
-                                wall->left, wall->top, wall->right,
-                                wall->bottom, &colX, &colY)) {
-        playdate->graphics->drawRect(colX, colY, 5, 5, kColorBlack);
+                              wall->left, wall->top, wall->right, wall->bottom,
+                              &tempColX, &tempColY, &tempDistance)) {
+        if (tempDistance < distance) {
+          distance = tempDistance;
+          colX = tempColX;
+          colY = tempColY;
+        }
       }
+    }
+    if (distance < FOV_LENGTH) {
+      playdate->graphics->drawRect(colX, colY, 5, 5, kColorBlack);
+      playdate->graphics->drawLine(playerX, playerY, round(colX), round(colY),
+                                   1, kColorBlack);
+    } else {
+      playdate->graphics->drawLine(playerX, playerY, round(distanceX),
+                                   round(distanceY), 1, kColorBlack);
     }
 
     startAngle += rayStepAngle;
@@ -123,10 +132,10 @@ static int normalizeAngle(int angle) {
   return angle;
 }
 
-static bool getLineIntersection(float p0_x, float p0_y, float p1_x,
-                                  float p1_y, float p2_x, float p2_y,
-                                  float p3_x, float p3_y, float *i_x,
-                                  float *i_y) {
+static bool getLineIntersection(float p0_x, float p0_y, float p1_x, float p1_y,
+                                float p2_x, float p2_y, float p3_x, float p3_y,
+                                float *i_x, float *i_y,
+                                float *intersectionDistance) {
   float s1_x, s1_y, s2_x, s2_y;
   s1_x = p1_x - p0_x;
   s1_y = p1_y - p0_y;
@@ -145,6 +154,7 @@ static bool getLineIntersection(float p0_x, float p0_y, float p1_x,
     // s1_x);
     *i_x = p0_x + (t * s1_x);
     *i_y = p0_y + (t * s1_y);
+    *intersectionDistance = sqrt(pow(*i_x - p0_x, 2) + pow(*i_y - p0_y, 2));
     return true;
   }
 
